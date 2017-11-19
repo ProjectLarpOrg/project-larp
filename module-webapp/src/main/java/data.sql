17879d3e508b59f612e4b759115b86f4440252e1
Damien FREMONT <damien.fremont@gmail.com>
2017-11-19 20:22:14 +0100

    init project
    fix auth test pass

--- module-webapp/src/main/java/data.sql
+++ module-webapp/src/main/java/data.sql
@@ -1,4 +1,4 @@
 DELETE FROM user WHERE id=1;
 INSERT INTO user (id, username, password, enabled, account_Non_Expired, credentials_Non_Expired, account_Non_Locked) VALUES
-  ('1','admin', 'admin', true, true, true, true)
+  ('1','admin', '$2a$10$s32PUU1QHIVpmscX1O4.F.bAttAS1X2XvZ/iSYtHOY2QK8iMpgs9C', true, true, true, true)
 ;
---
+++ module-webapp/src/test/java/com/projectlarp/app/common/search/EncodeTest.java
@@ -0,0 +1,16 @@
+package com.projectlarp.app.common.search;
+
+import org.junit.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class EncodeTest {
+
+	@Test
+	public void test() {
+		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String hashedPassword = passwordEncoder.encode("admin");
+		System.out.println(hashedPassword);
+	}
+	
+}
